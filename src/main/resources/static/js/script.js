61b7202389555010fce6545a3963bbba29fcee73
Nguyen Duc Nghia <ducnghiabachong@gmail.com>
2026-02-06 22:40:23 +0700

    them_modal_trang_login

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,20 +1,51 @@
-const openBtn = document.getElementById("openLogin");
-const modal = document.getElementById("loginModal");
-const overlay = document.getElementById("overlay");
-const closeBtn = document.querySelector(".close");
+const links = document.querySelectorAll(".nav a");
+const currentPath = window.location.pathname.split("/").pop();
 
-openBtn.onclick = () => {
-    modal.style.display = "block";
+links.forEach(link => {
+    if (link.getAttribute("href") === currentPath) {
+        link.classList.add("active");
+    }
+});
+
+
+const overlay = document.getElementById("overlay");
+
+const loginModal = document.getElementById("loginModal");
+const contactModal = document.getElementById("contactModal");
+
+const openLogin = document.getElementById("openLogin");
+const openContact = document.getElementById("contactUs");
+
+const closeLogin = document.getElementById("closeLogin");
+const closeContact = document.getElementById("closeContact");
+
+// Open login
+openLogin.onclick = () => {
+    loginModal.style.display = "block";
     overlay.style.display = "block";
 };
 
+// Open contact
+openContact.onclick = () => {
+    contactModal.style.display = "block";
+    overlay.style.display = "block";
+};
+
+// Close login
+closeLogin.onclick = () => {
+    loginModal.style.display = "none";
+    overlay.style.display = "none";
+};
+
+// Close contact
+closeContact.onclick = () => {
+    contactModal.style.display = "none";
+    overlay.style.display = "none";
+};
+
+// Click overlay to close all
 overlay.onclick = () => {
-    modal.style.display = "none";
+    loginModal.style.display = "none";
+    contactModal.style.display = "none";
     overlay.style.display = "none";
 };
-
-closeBtn.onclick = () => {
-    modal.style.display = "none";
-    overlay.style.display = "none";
-};
-
