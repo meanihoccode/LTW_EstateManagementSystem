ca527713a65e44c761433c5645f676dd8cf195fb
Nguyen Duc Nghia <ducnghiabachong@gmail.com>
2026-02-09 01:30:29 +0700

    tenants_backend

--- src/main/java/com/example/ltw_quanlybds/api/TenantController.java
+++ src/main/java/com/example/ltw_quanlybds/api/TenantController.java
@@ -1,1 +1,46 @@
+package com.example.ltw_quanlybds.api;
+
+import com.example.ltw_quanlybds.entity.Tenant;
+import com.example.ltw_quanlybds.service.TenantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tenants")
+@CrossOrigin(origins = "*")
+public class TenantController {
+    @Autowired
+    private TenantService tenantService;
+    @GetMapping
+    public ResponseEntity<List<Tenant>> getAll() {
+        return ResponseEntity.ok(tenantService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tenant> getById( @PathVariable Integer id) {
+        return ResponseEntity.ok(tenantService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Tenant> createTenant(@RequestBody Tenant tenant) {
+        Tenant createdTenant = tenantService.createTenant(tenant);
+        return ResponseEntity.status(201).body(createdTenant);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Tenant> updateTenant(@PathVariable Integer id, @RequestBody Tenant tenant) {
+        tenant.setId(id);
+        Tenant updatedTenant = tenantService.updateTenant(tenant);
+        return ResponseEntity.ok(updatedTenant);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTenant(@PathVariable Integer id) {
+        tenantService.deleteTenant(id);
+        return ResponseEntity.noContent().build();
+    }
+}
 
--- src/main/java/com/example/ltw_quanlybds/repository/TenantRepository.java
+++ src/main/java/com/example/ltw_quanlybds/repository/TenantRepository.java
@@ -1,1 +1,13 @@
+package com.example.ltw_quanlybds.repository;
 
+import com.example.ltw_quanlybds.entity.Tenant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TenantRepository  extends JpaRepository<Tenant,Integer> {
+    List<Tenant> findByPhone(Integer contractId);
+
+}
