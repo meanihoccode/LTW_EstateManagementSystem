d19fec87bdbb8716c3f3213d57fd3cb6c2464702
Nguyen Duc Nghia <ducnghiabachong@gmail.com>
2026-04-10 13:01:58 +0700

    ud_payments

--- src/main/java/com/example/ltw_quanlybds/repository/TenantRepository.java
+++ src/main/java/com/example/ltw_quanlybds/repository/TenantRepository.java
@@ -5,9 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 @Repository
 public interface TenantRepository  extends JpaRepository<Tenant,Integer> {
     List<Tenant> findByPhone(Integer contractId);
 
+    @Query("SELECT t FROM Tenant t WHERE " +
+            "(:keyword IS NULL OR :keyword = '' OR " +
+            "t.fullName LIKE CONCAT('%', :keyword, '%') OR " +
+            "t.phone LIKE CONCAT('%', :keyword, '%') OR " +
+            "t.email LIKE CONCAT('%', :keyword, '%') OR " +
+            "t.idNumber LIKE CONCAT('%', :keyword, '%'))")
+    Page<Tenant> searchTenants(@Param("keyword") String keyword, Pageable pageable);
 }
